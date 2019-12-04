8cde328ba9d6270827c29c5588a1a30b3820d6d3
blackscalare <blackscalare@gmail.com>
2019-12-04 12:35:31 +0100

    1

--- aoc2019/day3/part1.py
+++ aoc2019/day3/part1.py
@@ -1,111 +1,73 @@
-with open('input.txt', 'r') as f:
+with open('testinput2.txt', 'r') as f:
 	lines = f.readlines()
 
 line1 = lines[0].split(',')
 line2 = lines[1].split(',')
-posX = 0
-lastPosX = 0
-lastPosY = 0
-posY = 0
-posX1 = 0
-posY1 = 0
-lastPosX1 = 0
-lastPosY1 = 0
-distance = []
-distance1 = []
 
-for a in line1:
-	if a.startswith('R'):
-		a = a.split('R')
-		lastPosX = posX
-		posX += int(a[1])
-	elif a.startswith('U'):
-		a = a.split('U')
-		lastPosY = posY
-		posY += int(a[1])
-	elif a.startswith('L'):
-		a = a.split('L')
-		lastPosX = posX
-		posX -= int(a[1])
-	elif a.startswith('D'):
-		a = a.split('D')
-		lastPosY = posY
-		posY -= int(a[1])
-		
-	"""if lastPosX != posX:
-		if posX < lastPosX:
-			zz = lastPosX
-			xx = posX
-		else:"""
-	zz = posX
-	xx = lastPosX
-	#distance.append([list(range(xx, zz+1)), posY])
-	for x in range(xx, zz+1):
-		distance.append([x, posY])
-		#distance.append([x, posY] for x in range(xx, zz+1))
-		#distance.append([[x, posY] for x in range(xx, zz+1)])
-	"""elif lastPosY != posY:
-		if posY < lastPosY:
-			zz = lastPosY
-			xx = posY
-		else:"""
-	zz = posY
-	xx = lastPosY
-		
-	for x in range(xx, zz+1):
-		distance.append([posX, x])
-		#distance.append([posX, list(range(xx, zz+1))])
-		#distance.append(list(zip(distance, [[posX, x] for x in range(xx, zz+1)])))
+def distance_calc(line):
+	dx = 0
+	dy = 0
+	distance = []
+	for a in line:
+		if a.startswith('R'):
+			dx += int(a[1:])
+		elif a.startswith('U'):
+			dy += int(a[1:])
+		elif a.startswith('L'):
+			dx -= int(a[1:])
+		elif a.startswith('D'):
+			dy -= int(a[1:])
+		distance.append([dx, dy])
+	return distance
 
+def all_points(distance):
+	allPoints = []
+	lastPoint = [0, 0]
+	for point in distance:
+		reverse = 1
+		if point[0] != lastPoint[0]:
+			if lastPoint[0] > point[0]:
+				reverse = -1
+			allPoints.append([list(range(lastPoint[0], point[0] + reverse, reverse)), lastPoint[1]])
+		else:
+			if lastPoint[1] > point[1]:
+				reverse = -1
+			allPoints.append([lastPoint[0], list(range(lastPoint[1], point[1] + reverse, reverse))])
+		lastPoint = point
+	
+	return allPoints
+	
+def find_intersections(allPoints, allPoints1):
+	steps = len(allPoints)
+	for x in allPoints:
+		print('Steps left: {}'.format(steps))
+		for y in allPoints1:
+			if type(x[0]) == list:
+				for i in x[0]:
+					if type(y[0]) == list:
+						for j in y[0]:
+							if i == y[0] and x[1] == j:
+								print('X')
+		steps -= 1
+	
+	
+distance = distance_calc(line1)
+distance1 = distance_calc(line2)
+allPoints = all_points(distance)
+allPoints1 = all_points(distance1)
 #print(distance)
+#print(allPoints)
+intersections = find_intersections(allPoints, allPoints1)
 
-for a in line2:
-	if a.startswith('R'):
-		a = a.split('R')
-		lastPosX1 = posX1
-		posX1 += int(a[1])
-	elif a.startswith('U'):
-		a = a.split('U')
-		lastPosY1 = posY1
-		posY1 += int(a[1])
-	elif a.startswith('L'):
-		a = a.split('L')
-		lastPosX1 = posX1
-		posX1 -= int(a[1])
-	elif a.startswith('D'):
-		a = a.split('D')
-		lastPosY1 = posY1
-		posY1 -= int(a[1])
-		
-	"""if lastPosX1 != posX1:
-		if posX1 < lastPosX1:
-			zz = lastPosX1
-			xx = posX1
-		else:"""
-	zz = posX1
-	xx = lastPosX1
-	#distance1.append([list(range(xx, zz+1)), posY1])
-	for x in range(xx, zz+1):
-		distance1.append([x, posY1])
-		#distance1.append([[x, posY1] for x in range(xx, zz+1)])
-	"""elif lastPosY1 != posY1:
-		if posY1 < lastPosY1:
-			zz = lastPosY1
-			xx = posY1
-		else:"""
-	zz = posY1
-	xx = lastPosY1
-		
-	#distance1.append([posX1, list(range(xx, zz+1))])
-	for x in range(xx, zz+1):
-		distance1.append([posX1, x])
-		#distance1.append(list(zip(distance, [[posX1, x] for x in range(xx, zz+1)])))
+#print(allPoints)
+#print(allPoints1)
+#print(distance)
 #print(distance1)
-
-for x in distance:
-	if x in distance1:
+print(intersections)
+for x in allPoints:
+	if x in allPoints1:
 		ans = abs(0-x[0])+abs(0-x[1])
 		print(ans)
 
-print('End position\nx: {}, y: {}'.format(posX, posY))
-print('End position 2\nx: {}, y: {}'.format(posX1, posY1))
+print('End position\n{}'.format(distance[len(distance) - 1]))
+print('End position 2\n{}'.format(distance1[len(distance) - 1]))
