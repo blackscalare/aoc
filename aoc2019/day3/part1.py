358d7a2a16157139315e44f174a2c0d0577032ba
blackscalare <blackscalare@gmail.com>
2019-12-10 14:29:07 +0100

    finally done with day3/part1 merged part1.py and part1_rework.py

--- aoc2019/day3/part1.py
+++ aoc2019/day3/part1.py
@@ -1,73 +1,73 @@
-with open('testinput2.txt', 'r') as f:
-	lines = f.readlines()
+with open('input.txt', 'r') as f:
+    lines = f.read().splitlines()
 
 line1 = lines[0].split(',')
 line2 = lines[1].split(',')
 
-def distance_calc(line):
-	dx = 0
-	dy = 0
-	distance = []
-	for a in line:
-		if a.startswith('R'):
-			dx += int(a[1:])
-		elif a.startswith('U'):
-			dy += int(a[1:])
-		elif a.startswith('L'):
-			dx -= int(a[1:])
-		elif a.startswith('D'):
-			dy -= int(a[1:])
-		distance.append([dx, dy])
-	return distance
+def get_intersections(points1, points2):
+    intersections = []
+    steps = []
+    steps_set = set()
+    steps_set2 = set()
+    steps2 = []
+    for i in range(len(points1)):
+        direction = 1
+        if i == 0:
+            continue
+        if points1[i][0] != points1[i - 1][0]:
+            if points1[i-1][0] > points1[i][0]:
+                direction = -1
+            for j in range(points1[i - 1][0], points1[i][0] + direction, direction):
+                steps_set.add((j, points1[i][1]))
+        elif points1[i][1] != points1[i - 1][1]:
+            if points1[i-1][1] > points1[i][1]:
+                direction = -1
+            for j in range(points1[i - 1][1], points1[i][1] + direction, direction):
+                steps_set.add((points1[i][0], j))
 
-def all_points(distance):
-	allPoints = []
-	lastPoint = [0, 0]
-	for point in distance:
-		reverse = 1
-		if point[0] != lastPoint[0]:
-			if lastPoint[0] > point[0]:
-				reverse = -1
-			allPoints.append([list(range(lastPoint[0], point[0] + reverse, reverse)), lastPoint[1]])
-		else:
-			if lastPoint[1] > point[1]:
-				reverse = -1
-			allPoints.append([lastPoint[0], list(range(lastPoint[1], point[1] + reverse, reverse))])
-		lastPoint = point
-	
-	return allPoints
-	
-def find_intersections(allPoints, allPoints1):
-	steps = len(allPoints)
-	for x in allPoints:
-		print('Steps left: {}'.format(steps))
-		for y in allPoints1:
-			if type(x[0]) == list:
-				for i in x[0]:
-					if type(y[0]) == list:
-						for j in y[0]:
-							if i == y[0] and x[1] == j:
-								print('X')
-		steps -= 1
-	
-	
-distance = distance_calc(line1)
-distance1 = distance_calc(line2)
-allPoints = all_points(distance)
-allPoints1 = all_points(distance1)
-#print(distance)
-#print(allPoints)
-intersections = find_intersections(allPoints, allPoints1)
+    for i in range(len(points2)):
+        direction = 1
+        if i == 0:
+            continue
+        if points2[i][0] != points2[i - 1][0]:
+            if points2[i-1][0] > points2[i][0]:
+                direction = -1
+            for j in range(points2[i - 1][0], points2[i][0] + direction, direction):
+                steps_set2.add((j, points2[i][1]))
+        elif points2[i][1] != points2[i - 1][1]:
+            if points2[i-1][1] > points2[i][1]:
+                direction = -1
+            for j in range(points2[i - 1][1], points2[i][1] + direction, direction):
+                steps_set2.add((points2[i][0], j))
+    for x in steps_set:
+        if x in steps_set2:
+            intersections.append(x)
+    return intersections
+    
+def get_points(line):
+    dx = 0
+    dy = 0
+    points = []
+    points.append([0,0])
+    for point in line:
+        if point.startswith('U'):
+            dx += int(point[1:])
+        elif point.startswith('R'):
+            dy += int(point[1:])
+        elif point.startswith('D'):
+            dx -= int(point[1:])
+        elif point.startswith('L'):
+            dy -= int(point[1:])
+        points.append([dx, dy])
+    return points
 
-#print(allPoints)
-#print(allPoints1)
-#print(distance)
-#print(distance1)
-print(intersections)
-for x in allPoints:
-	if x in allPoints1:
-		ans = abs(0-x[0])+abs(0-x[1])
-		print(ans)
+points1 = get_points(line1)
+points2 = get_points(line2)
+intersections = get_intersections(points1, points2)
 
-print('End position\n{}'.format(distance[len(distance) - 1]))
-print('End position 2\n{}'.format(distance1[len(distance) - 1]))
+lowest = 99999999
+for intersection in intersections:
+    ans = abs(0-intersection[0])+abs(0-intersection[1])
+    if ans < lowest and ans != 0:
+        lowest = ans
+print(lowest)
