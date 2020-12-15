67f5e74fa0516c5a62e774c455b6c7e2d39291df
blackscalare <blackscalare@gmail.com>
2020-12-15 17:53:23 +0100

    completed day15

--- aoc2020/day15/solution.py
+++ aoc2020/day15/solution.py
@@ -1,30 +1,30 @@
-starting_numbers = [line.rstrip('\n').split(',') for line in open('input.txt')]
+def run(starting_numbers, length):
+    numbers = []
+    for number in starting_numbers:
+        for n in number:
+            numbers.append(int(n))
+    previous_spoken = {}
+    last_spoken_number = spoken_number = 0
+    occurances = []
+    for i in range(len(numbers)):
+        last_spoken_number = numbers[i]
+        previous_spoken[last_spoken_number] = [i + 1]
 
-numbers = []
-for number in starting_numbers:
-    for n in number:
-        numbers.append(int(n))
-previous_spoken = {}
-last_spoken_number = spoken_number = 0
-occurances = []
-for i in range(len(numbers)):
-    last_spoken_number = numbers[i]
-    previous_spoken[last_spoken_number] = [i + 1, i + 1]
-    occurances.append(last_spoken_number)
+    for i in range(len(numbers) + 1, length+1):
+        if len(previous_spoken[last_spoken_number]) <= 1:
+            spoken_number = 0
+            previous_spoken[spoken_number].append(i)
+        else:
+            vals = previous_spoken[last_spoken_number]
+            spoken_number = vals[-1] - vals[-2]
+            try:
+                previous_spoken[spoken_number].append(i)
+            except:
+                previous_spoken[spoken_number] = [i]
+        last_spoken_number = spoken_number
+    print(spoken_number)
 
-for i in range(len(numbers) + 1, 30000001):
-    if occurances.count(last_spoken_number) <= 1:
-        spoken_number = 0
-        j = previous_spoken[spoken_number][1]
-        previous_spoken[spoken_number] = [j, i]
-    else:
-        vals = previous_spoken[last_spoken_number]
-        spoken_number = vals[1] - vals[0]
-        try:
-            j = previous_spoken[spoken_number][1]
-            previous_spoken[spoken_number] = [j, i]
-        except:
-            previous_spoken[spoken_number] = [i, i]
-    occurances.append(spoken_number)
-    last_spoken_number = spoken_number
-print(spoken_number)
+
+starting_numbers = [line.rstrip('\r\n').split(',') for line in open('input.txt')]
+run(starting_numbers, 2020)
+run(starting_numbers, 30000000)
