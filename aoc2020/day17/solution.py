915e9e893980bdea1194cc53fcc78b604f0c1b91
blackscalare <blackscalare@gmail.com>
2020-12-18 11:36:59 +0100

    completed part2

--- aoc2020/day17/solution.py
+++ aoc2020/day17/solution.py
@@ -1,42 +1,57 @@
 from itertools import product
 from collections import defaultdict
 
-lines = [line.rstrip('\n') for line in open('input.txt')]
-
-def check_neighbours(pos, active):
+def check_neighbours(pos, active, neighbours, dims):
     tmp_active = []   
     count = 0
-    directions = list(product([0, 1, -1], repeat=3))
-    directions.remove((0,)*3)
-    for x, y, z in directions:
-        p = (pos[0] + x, pos[1] + y, pos[2] + z)
-        neighbours[p] += 1
-        if p in active:
-            count += 1
-    if count == 2 or count == 3:
-        tmp_active.append(pos)
+    directions = list(product([0, 1, -1], repeat=dims))
+    directions.remove((0,)*dims)
+    if dims == 3:
+        for x, y, z in directions:
+            p = (pos[0] + x, pos[1] + y, pos[2] + z)
+            neighbours[p] += 1
+            if p in active:
+                count += 1
+        if count == 2 or count == 3:
+            tmp_active.append(pos)
+    elif dims == 4:
+        for x, y, z, w in directions:
+            p = (pos[0] + x, pos[1] + y, pos[2] + z, pos[3] + w)
+            neighbours[p] += 1
+            if p in active:
+                count += 1
+        if count == 2 or count == 3:
+            tmp_active.append(pos)
     return tmp_active
-active = []
-universe = {}
-for y, line in enumerate(lines):
-    for x, c in enumerate(line):
-        if c == '#':
-            active.append((x, y, 0))
 
-def removeDuplicates(lst):
-    return [t for t in (set(tuple(i) for i in lst))]
+def run(lines, dims):
+    active = []
+    universe = {}
+    for y, line in enumerate(lines):
+        for x, c in enumerate(line):
+            if c == '#':
+                if dims == 3:
+                    active.append((x, y, 0))
+                elif dims == 4:
+                    active.append((x, y, 0, 0))
 
-z = 0
-while z != 6:
-    neighbours = defaultdict(int)
-    tmp_active = []
-    for pos in active:
-        tmp_active.extend(check_neighbours(pos, active))
-    for n in neighbours:
-        if neighbours[n] == 3 and n not in active:
-            tmp_active.append(n)
-    active = removeDuplicates(tmp_active)
-    z += 1
+    def removeDuplicates(lst):
+        return [t for t in (set(tuple(i) for i in lst))]
+
+    z = 0
+    while z != 6:
+        neighbours = defaultdict(int)
+        tmp_active = []
+        for pos in active:
+            tmp_active.extend(check_neighbours(pos, active, neighbours, dims))
+        for n in neighbours:
+            if neighbours[n] == 3 and n not in active:
+                tmp_active.append(n)
+        active = removeDuplicates(tmp_active)
+        z += 1
+    return len(active)
 
 
-print(len(active))
+lines = [line.rstrip('\n') for line in open('input.txt')]
+print('part1', run(lines, 3))
+print('part2', run(lines, 4))
